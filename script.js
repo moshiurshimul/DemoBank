3f2c6d21d267c904418d1da2c6162d364dfcf46d
moshiurshimul <hello@moshiurshimul.com>
2020-07-23 06:25:30 +0600

    JS added

---
+++ script.js
@@ -0,0 +1,7 @@
+// Hiding the login section 
+
+let loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', function() {
+    document.getElementById('login-details').style.display = 'none';
+})
