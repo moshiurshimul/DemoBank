2867977fc7e82f3bd02ca3a3b9ff9b91f89c589a
moshiurshimul <hello@moshiurshimul.com>
2020-07-23 08:03:00 +0600

    adding deposit for user

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Hiding the login section 
+// Login Button Event Handler
 
 let loginBtn = document.getElementById('login-btn');
 
@@ -7,4 +7,26 @@ loginBtn.addEventListener('click', function() {
     document.getElementById('login-details').style.display = 'none';
     // displaying user transaction area
     document.getElementById('transaction-area').style.display = 'block';
+});
+
+// Deposit Amount Handle
+
+document.getElementById('depositBtn').addEventListener('click', ()=> {
+    // converting new deposit amount string to number
+    const depositAmount = document.getElementById('depositAmount').value; 
+    const depositAmountNumber = parseFloat(depositAmount);
+    
+    // Converting current deposit amount
+    const currentDeposit = document.getElementById('currentDeposit').innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+
+    // adding two deposit value
+    const totalDeposit = depositAmountNumber + currentDepositNumber;
+
+    // Showing to user
+    document.getElementById('currentDeposit').innerText = totalDeposit;
+    
+    // Make ready for input
+    document.getElementById('depositAmount').value = '';
+    document.getElementById('depositAmount').focus();
 })
