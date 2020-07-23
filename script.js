3683c798df1ac7693fe4035f0b6fcbda923f4dcd
moshiurshimul <hello@moshiurshimul.com>
2020-07-23 06:59:35 +0600

    user area update

--- script.js
+++ script.js
@@ -3,5 +3,8 @@
 let loginBtn = document.getElementById('login-btn');
 
 loginBtn.addEventListener('click', function() {
+    // hiding login area
     document.getElementById('login-details').style.display = 'none';
+    // displaying user transaction area
+    document.getElementById('transaction-area').style.display = 'block';
 })
